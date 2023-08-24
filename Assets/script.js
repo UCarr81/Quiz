ffabcc88bf1cc789819bafd90358fb598543d47c
Uriel <CarrilloUriel81@gmail.com>
2023-08-23 22:04:29 -0700

    Added Functional Timer

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,18 @@
 var timer = document.querySelector('h1');
-var timerSecond = 120;
+var timerSecond = 6;
 
 timer.innerHTML = timerSecond;
+
+var countDown = setInterval(() => {
+ timerSecond--;
+ timer.innerHTML = timerSecond;
+ if(timerSecond <= 0 || timerSecond < 1) {
+    endTime();
+    clearInterval(countDown);
+    
+ }
+},1000)
+
+function endTime() {
+    timer.innerHTML = "TIME OUT"
+}
