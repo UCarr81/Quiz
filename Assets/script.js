b6fc3bdd080399f50a41d6872f96bacb79c1f668
Uriel <CarrilloUriel81@gmail.com>
2023-08-23 22:34:23 -0700

    Updated HTML

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,5 @@
 var timer = document.querySelector('h1');
-var timerSecond = 6;
+var timerSecond = 120;
 
 timer.innerHTML = timerSecond;
 
@@ -15,4 +15,22 @@ var countDown = setInterval(() => {
 
 function endTime() {
     timer.innerHTML = "TIME OUT"
+    alert("TIME OUT");
 }
+
+var questions = [
+    {
+        prompt: "Question 1 \n(A) Answer 1\n(B) Answer 2\n(C) Answer 3\n(D) Answer 4",
+
+        answer: "A"
+    }
+]
+//var Question = document.querySelector('h2')
+//var quizQuestion = questions
+//for (var i = 0; i < questions.length; i++) {
+//var reponse = window.prompt(questions[i].prompt)
+//if(response == questions[i].answer) {
+//    score++;
+//    alert("Correct")
+//}
+//}
